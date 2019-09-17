513b29ccfdf308024125692b6eaf08c1f4abfb13
Pignataro67 <Pignataro67@gmail.com>
2019-09-17 12:53:19 -0700

    setting up routes and search controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '/search_destination/:input', to: 'search#search_destination'
+  get '/confirm_route/convert_coords/:location', to: 'confirm_route#get_coords'
+  get '/confirm_route/mapbox', to: 'confirm_route#mapbox'
+  get '/lyft_uber', to: 'lyft_uber#uber_estimate'
 end
