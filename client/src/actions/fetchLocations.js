8ee5a8e6526f7c5a294d1888d581a91b6c58f13e
Pignataro67 <Pignataro67@gmail.com>
2019-09-21 11:28:46 -0700

    more last of the actions for now

--- client/src/actions/fetchLocations.js
+++ client/src/actions/fetchLocations.js
@@ -23,7 +23,19 @@ function _convertDropoffLatLong(location) {
     return _getLatLong(location).then(({ lat, long }) => dispatch({type: 'RETRIEVE_DROPOFF_LAT_LONG', dropoffLat: lat, dropoffLong: long}))
   }
 }
+
+function _normalizeUber({ prices }) {
+  return prices.map(({ display_name, estimate }) => (
+    {type: display_name, costEstimate: estimate}
+  ))
+}
   
+function _normalizeLyft({ cost_estimates }) {
+  return cost_estimates.map(({ display_name, estimated_cost_cents_min, estimated_cost_cents_max }) => (
+    {type: display_name, costEstimate: `${estimated_cost_cents_min / 100} - ${estimated_cost_cents_max / 100}`}
+  ))
+}
+
 export function fetchPickupLocation(input) {
   console.log(input)
     return (dispatch) => {
@@ -54,7 +66,9 @@ export function fetchUberEstimate(pickupLat, pickupLong, dropoffLat, dropoffLong
   return (dispatch) => {
     fetch(`RailsApi/uber?pickupLat=${pickupLat}&pickupLong=${pickupLong}&dropoffLat=${dropoffLat}&dropoffLong=${dropoffLong}`)
     .then(res => res.json())
-    .then(data => dispatch({ type: 'ADD_UBER_ESTIMATES_TO_STATE', estimate: data.prices}))
+    .then(data => _normalizeUber(data))
+    .then(estimates => estimates.reverse().slice(1))
+    .then(estimates => dispatch({ type: 'ADD_UBER_ESTIMATES_TO_STATE', estimates: estimates }))
   };
 }
   
@@ -63,7 +77,8 @@ export function fetchLyftEstimate(pickupLat, pickupLong, dropoffLat, dropoffLong
     dispatch({ type: 'FETCHING_LYFT_ESTIMATE'});
     fetch(`RailsApi/lyft?pickupLat=${pickupLat}&pickupLong=${pickupLong}&dropoffLat=${dropoffLat}&dropoffLong=${dropoffLong}`)
     .then(res => res.json())
-    .then(data => dispatch({ type: 'ADD_LYFT_ESTIMATES_TO_STATE', estimate: data.cost_estimates}))
+    .then(data => _normalizeLyft(data))
+    .then(estimates => dispatch({ type: 'ADD_LYFT_ESTIMATES_TO_STATE', estimates: estimates }))
   };
 }
 
