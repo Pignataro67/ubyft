273ee2af1733286c87408943fd9644c831b56a11
Pignataro67 <Pignataro67@gmail.com>
2019-09-22 04:59:04 -0700

    adding table jsx func.

--- client/src/components/results/Result.js
+++ client/src/components/results/Result.js
@@ -1,13 +1,33 @@
 import React, { Component } from 'react';
+import { Header, Table } from 'semantic-ui-react';
+import Button from '../Button';
 import Ride from './Ride';
 
 class Result extends Component {
-  render() {
-    return (
-      <div>
+  tableRows(estimates) {
+    return estimates.map(({type, costEstimate}) => (
+      <Table.Row><Table.Cell>{type}</Table.Cell> <Table.Cell>{costEstimate}</Table.Cell></Table.Row>
+      ))
+    }
 
-      </div>
-    )
+    render() {
+    return (
+        <React.Fragment>
+        <h3>{this.props.header}</h3>
+      	  <Table collapsing celled selectable>
+            <Table.Header>
+              <Table.Row>
+                <Table.HeaderCell>Ride Type</Table.HeaderCell>
+                <Table.HeaderCell>Cost</Table.HeaderCell>
+              </Table.Row>
+            </Table.Header>
+            <Table.Body>
+              {this.tableRows(this.props.estimates)}
+            </Table.Body>
+          </Table>
+          <Button buttonTitle={this.props.buttonTitle}/>
+      </React.Fragment>
+    );
   }
 }
 
