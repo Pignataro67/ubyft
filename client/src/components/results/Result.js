7fca822c55767e5dba01be577890e7ae841716fc
Pignataro67 <Pignataro67@gmail.com>
2019-09-28 04:26:55 -0700

    adding state and click event

--- client/src/components/results/Result.js
+++ client/src/components/results/Result.js
@@ -4,6 +4,30 @@ import Button from '../Button';
 import Ride from './Ride';
 
 class Result extends Component {
+
+  state = {
+    estimates: []
+  }
+
+  componentWillReceiveProps() {
+    const estimates = this.props.estimates.map(({type, costEstimate}) => ({type: type, costEstimate: costEstimate, counter: 0}))
+
+    this.setState({
+      estimates
+    })
+  }
+
+  handleOnClick = (idx) => {
+    let estimate = this.state.estimates[idx]
+    let estimates = this.state.estimates
+
+    estimates.splice(idx, 1, {type: estimate.type, costEstimate: estimate.costEstimate, counter: estimate.counter + 1})
+
+    this.setState({
+      estimates
+    })
+  }
+
   tableRows(estimates) {
     return estimates.map(({type, costEstimate}) => (
       <Table.Row><Table.Cell>{type}</Table.Cell> <Table.Cell>{costEstimate}</Table.Cell></Table.Row>
